96e918e256e313c592e8eaf6ea4610bbcc67ab20
Ch0der <eric@imagedisplay.com>
2019-08-15 16:26:00 -0500

    Initial commit

---
+++ lib/app_checklist.dart
@@ -0,0 +1,49 @@
+// primary objective is a workout builder app that smoothly builds list into readable format that is
+// globally identical to allow easy sharing of workouts without confusion - customization is limited in order to maintain simple formatting
+// implement a rating system to allow users to vote on workouts and sort according to intensity/enjoyable/relevence/specific body part
+// vision is user can look at their phone screen and feel as comfortable as looking at their own notebook, it should not feel like
+// a chore to look at workout list, should be as easy to read and understand as possible
+// later goal is to introduce a product area in which users can easily purchase related items - should be done in a way that feels
+// non burdening to user -
+// later later goal - area for food/diet - most likely not happening
+// hearthstone deck approach - keep the workout storage limited so it doesnt become a huge collection with no use
+// option when selecting other w/os to either download or 'do now', which automatically builds workout and makes it current activated
+// body selection screen
+// login screen
+// view workout screen
+// edit workout screen
+// upload workout
+// download workout
+// create workout screen
+// local db for offline workouts
+// db for uploading/downloading (firestore?)
+// user search lookup
+// workout search/lookup (specific codes to share with friends?)
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
