d3020bcc15a355a0ef8ba2db0a398b6c2388a2a0
Ch0der <eric@imagedisplay.com>
2019-10-10 17:08:09 -0500

    padscreen

--- lib/screens/pad_demo.dart
+++ lib/screens/pad_demo.dart
@@ -16,7 +16,21 @@ class _PadDemoState extends State<PadDemo> {
   buildBody() {
     return Container(
       decoration: BoxDecoration(),
-      child: buildNotebook(),
+      child: Stack(
+        children: <Widget>[
+          buildNotebook(),
+          Positioned(
+            left: 70,
+            top: 80,
+            child: textTest(),
+          ),
+          Positioned(
+            left: 70,
+            top: 115,
+            child: text2(),
+          ),
+        ],
+      ),
     );
   }
 
@@ -24,7 +38,35 @@ class _PadDemoState extends State<PadDemo> {
     return Stack(
       children: <Widget>[
         blueLines(),
-        hinges(),
+        Positioned(
+          top: -162,
+          left: -14,
+          child: hinges(),
+        ),
+        Positioned(
+          left: 60,
+          child: redLines(),
+        ),
+      ],
+    );
+  }
+
+  redLines() {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 1,
+          height: 1000,
+          color: Colors.red[200].withOpacity(.8),
+        ),
+        Container(
+          width: 3,
+        ),
+        Container(
+          width: 1,
+          height: 1000,
+          color: Colors.red[200].withOpacity(.8),
+        )
       ],
     );
   }
@@ -71,44 +113,216 @@ class _PadDemoState extends State<PadDemo> {
   }
 
   hinges() {
-    return (Column(
-      children: <Widget>[
-        Padding(padding: EdgeInsets.only(top: 50),),
-        hinge(),
-      ],
-    ));
+    final double spacing = 40;
+    return Transform.scale(
+      scale: .7,
+      child: (Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+          Padding(
+            padding: EdgeInsets.only(top: spacing),
+          ),
+          hinge(),
+        ],
+      )),
+    );
   }
 
   hinge() {
     return Stack(
+      overflow: Overflow.visible,
       children: <Widget>[
+        Positioned(
+          bottom: -2.5,
+          left: 38,
+          child: Container(
+            height: 17,
+            width: 7,
+            decoration: BoxDecoration(
+              color: Colors.black87.withOpacity(.3),
+              borderRadius: BorderRadius.all(
+                Radius.circular(4),
+              ),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black87.withOpacity(.4),
+                    spreadRadius: 3,
+                    blurRadius: 1)
+              ],
+            ),
+          ),
+        ),
         Column(
           children: <Widget>[
             Container(
               height: 6,
               width: 45,
               decoration: BoxDecoration(
-                color: Colors.black87,
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.grey[600],
+                    Colors.grey[300],
+                    Colors.grey,
+                  ],
+                  stops: [
+                    0,
+                    .5,
+                    1,
+                  ],
+                ),
                 borderRadius: BorderRadius.only(
                   topRight: Radius.circular(5),
                   bottomRight: Radius.circular(5),
                 ),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black87.withOpacity(.3),
+                      offset: Offset(0, 1),
+                      blurRadius: 2)
+                ],
               ),
             ),
-            Padding(padding: EdgeInsets.only(top: 3),),
+            Padding(
+              padding: EdgeInsets.only(top: 3),
+            ),
             Container(
               height: 6,
               width: 45,
               decoration: BoxDecoration(
-                color: Colors.black87,
+                gradient: LinearGradient(colors: [
+                  Colors.grey[600],
+                  Colors.grey[300],
+                  Colors.grey,
+                ], stops: [
+                  0,
+                  .5,
+                  1
+                ]),
                 borderRadius: BorderRadius.only(
                   topRight: Radius.circular(5),
                   bottomRight: Radius.circular(5),
                 ),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black87.withOpacity(.3),
+                      offset: Offset(0, 1),
+                      blurRadius: 2,
+                      spreadRadius: 2)
+                ],
               ),
-            )
-
+            ),
           ],
+        ),
+      ],
+    );
+  }
+
+  textTest() {
+    return Column(
+      children: <Widget>[
+        Container(
+          child: Text(
+            'Description           Set  Reps   Wt',
+            style: TextStyle(fontFamily: 'Handlee', fontSize: 25,decoration: TextDecoration.underline),
+          ),
+        )
+      ],
+    );
+  }
+
+  text2() {
+    return Column(
+      children: <Widget>[
+        Container(
+          child: Text(
+            'Deadlift                          1        10         50',
+            style: TextStyle(fontFamily: 'Handlee', fontSize: 21),
+          ),
+        ),
+        Container(
+          child: Text(
+            '      ~                                2        10         50',
+            style: TextStyle(fontFamily: 'Handlee', fontSize: 21),
+          ),
+        ),
+        Container(
+          child: Text(
+            '      ~                                3        10         50',
+            style: TextStyle(fontFamily: 'Handlee', fontSize: 21),
+          ),
+        ),
+        Container(
+          child: Text(
+            '      ~                                4        10         50',
+            style: TextStyle(fontFamily: 'Handlee', fontSize: 21),
+          ),
         ),
       ],
     );
