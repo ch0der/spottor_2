209f537e0cd7218149e04f32835df75b7ea7f64b
Eric <eric@imagedisplay.com>
2019-11-01 02:07:37 -0500

    test

--- lib/screens/editSelection.dart
+++ lib/screens/editSelection.dart
@@ -8,19 +8,48 @@ class EditSelector extends StatefulWidget {
 }
 
 final List<Image> imgList = [
-  Image(image: AssetImage('assets/imgs/stockphotos/workout1.jpg'),),
-  Image(image: AssetImage('assets/imgs/stockphotos/workout2.jpg'),),
-  Image(image: AssetImage('assets/imgs/stockphotos/workout3.jpg'),),
-  Image(image: AssetImage('assets/imgs/stockphotos/workout1.jpg'),),
-  Image(image: AssetImage('assets/imgs/stockphotos/workout2.jpg'),),
+  Image(
+    image: AssetImage('assets/imgs/stockphotos/workout1.jpg'),
+    fit: BoxFit.fill,
+  ),
+  Image(
+    image: AssetImage('assets/imgs/stockphotos/workout2.jpg'),
+    fit: BoxFit.fill,
+  ),
+  Image(
+    image: AssetImage('assets/imgs/stockphotos/workout3.jpg'),
+    fit: BoxFit.fill,
+  ),
+  Image(
+    image: AssetImage('assets/imgs/stockphotos/workout1.jpg'),
+    fit: BoxFit.fill,
+  ),
+  Image(
+    image: AssetImage('assets/imgs/stockphotos/workout2.jpg'),
+    fit: BoxFit.fill,
+  ),
 ];
 final List<Widget> imgList2 = [
-  Image(image: AssetImage('assets/imgs/stockphotos/food1.jpg'),),
-  Image(image: AssetImage('assets/imgs/stockphotos/food2.jpg'),),
-  Image(image: AssetImage('assets/imgs/stockphotos/food3.jpg'),),
-  Image(image: AssetImage('assets/imgs/stockphotos/food1.jpg'),),
-  Image(image: AssetImage('assets/imgs/stockphotos/food2.jpg'),),
-
+  Image(
+    image: AssetImage('assets/imgs/stockphotos/food1.jpg'),
+    fit: BoxFit.fill,
+  ),
+  Image(
+    image: AssetImage('assets/imgs/stockphotos/food2.jpg'),
+    fit: BoxFit.fill,
+  ),
+  Image(
+    image: AssetImage('assets/imgs/stockphotos/food3.jpg'),
+    fit: BoxFit.fill,
+  ),
+  Image(
+    image: AssetImage('assets/imgs/stockphotos/food1.jpg'),
+    fit: BoxFit.fill,
+  ),
+  Image(
+    image: AssetImage('assets/imgs/stockphotos/food2.jpg'),
+    fit: BoxFit.fill,
+  ),
 ];
 int playDuration = 4;
 
@@ -34,74 +63,91 @@ class _EditSelectorState extends State<EditSelector> {
           Column(
             children: <Widget>[
               Expanded(
-                  child: CarouselSlider(
-                autoPlay: true,
-                viewportFraction: 1.0,
-                autoPlayInterval: Duration(milliseconds: 5000),
-                aspectRatio: MediaQuery.of(context).size.aspectRatio,
-                height: 700,
-                items: imgList.map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                        child: Transform.scale(
-                          scale: 1.15,
-                          child: i,
-                        ),
-                      );
-                    },
-                  );
-                }).toList(),
-              )),
-              Expanded(
-                  child: Opacity(opacity: .6,
-                    child: CarouselSlider(
-                autoPlay: true,
-                viewportFraction: 1.0,
-                initialPage: 3,
-                autoPlayInterval: Duration(seconds: playDuration),
-                aspectRatio: MediaQuery.of(context).size.aspectRatio,
-                height: 700,
-                items: imgList2.map((i) {
-                    return Builder(
-                      builder: (BuildContext context) {
-                        return Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                          ),
-                          child: Transform.scale(
-                            scale: 1.15,
-                            child: i,
-                          ),
-                        );
-                      },
-                    );
-                }).toList(),
+                child: Stack(
+                  children: <Widget>[
+                    CarouselSlider(
+                      autoPlay: true,
+                      viewportFraction: 1.0,
+                      height: 1000,
+                      autoPlayInterval: Duration(milliseconds: 5000),
+                      aspectRatio: MediaQuery.of(context).size.aspectRatio,
+                      items: imgList.map(
+                        (i) {
+                          return Builder(
+                            builder: (BuildContext context) {
+                              return Container(
+                                child: i,
+                              );
+                            },
+                          );
+                        },
+                      ).toList(),
+                    ),
+                    selectionText('Workouts'),
+                  ],
+                ),
               ),
-                  )),
+              Expanded(
+                child: Stack(
+                  children: <Widget>[
+                    Opacity(
+                      opacity: .6,
+                      child: CarouselSlider(
+                        autoPlay: true,
+                        viewportFraction: 1.0,
+                        initialPage: 3,
+                        autoPlayInterval: Duration(seconds: playDuration),
+                        height: 1000,
+                        items: imgList2.map(
+                          (i) {
+                            return Builder(
+                              builder: (BuildContext context) {
+                                return Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                  ),
+                                  child: i,
+                                );
+                              },
+                            );
+                          },
+                        ).toList(),
+                      ),
+                    ),
+                    selectionText('Meal Plan'),
+                  ],
+                ),
+              ),
             ],
           ),
-          Positioned.fill(
-            top: 250,
-            child: Align(
-              alignment: Alignment.center,
-              child: Container(
-                height: 100,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.black87.withOpacity(.5),
-                ),
-                child: Center(
-                    child: Text(
-                  'Meal Plan',
+        ],
+      ),
+    );
+  }
+  selectionText(String text){
+    return Align(
+      alignment: Alignment.center,
+      child: Material(
+        color: Colors.black87.withOpacity(.4),
+        child: RaisedButton(
+          splashColor: Colors.green.withOpacity(.25),
+          onPressed: (){
+          },
+          color: Colors.black87.withOpacity(.25),
+          child: Container(
+            height: 80,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+            ),
+            child: Center(
+                child: Text(
+                  text,
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                      fontSize: 60, color: Colors.white.withOpacity(.8)),
+                      fontSize: 60, color: Colors.white.withOpacity(.9)),
                 )),
-              ),
-            ),
           ),
-        ],
+        ),
       ),
     );
   }
