6db1b46b9ab0ac756953f5f9ee0417340500d31c
Ch0der <eric@imagedisplay.com>
2019-11-06 15:44:38 -0600

    building editor5

--- lib/screens/testing_3d_thing.dart
+++ lib/screens/testing_3d_thing.dart
@@ -1,37 +1,151 @@
+import 'dart:ui' as prefix0;
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:spottor_2/screens/pad.dart';
 import 'package:flutter_page_indicator/flutter_page_indicator.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
-class S02E02 extends StatelessWidget {
+class S02E02 extends StatefulWidget {
+  @override
+  _S02E02State createState() => _S02E02State();
+}
 
+class _S02E02State extends State<S02E02> {
   final List imgList = [
     'assets/imgs/stockphotos/workout1.jpg',
     'assets/imgs/stockphotos/workout2.jpg',
     'assets/imgs/stockphotos/workout3.jpg'
   ];
+
   final List bookList = [
-   padpenis(day: 'monday',),
-    padpenis(day: 'teus',),
+    padpenis(
+      day: 'monday',
+    ),
+    padpenis(
+      day: 'teus',
+    ),
   ];
+  String dayOfWeekQ;
+  String bodyPartQ;
 
   @override
   Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        searchFilter(),
+        Container(
+          height: 150,
+        ),
+        swiperText(),
+      ],
+    );
+  }
+
+  searchFilter() {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 100,
+          child: Text(
+            'FILTER BY',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 15),
+          ),
+        ),
+        Container(
+          width: 125,
+          child: DropdownButtonFormField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.only(bottom: 2),
+                labelText: 'Day',
+              ),
+              items: <String>[
+                'Sunday',
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday'
+              ].map<DropdownMenuItem<String>>(
+                (String value) {
+                  return DropdownMenuItem(
+                    child: Text(value),
+                    value: value,
+                  );
+                },
+              ).toList(),
+              onChanged: (value) {
+                setState(() {
+                  dayOfWeekQ = value;
+                });
+              },
+              value: dayOfWeekQ),
+        ),
+        Container(
+          width: 20,
+        ),
+        bodyPartDetail(),
+      ],
+    );
+  }
+  swiperText(){
     return Swiper(
       itemBuilder: (BuildContext context, int index) {
         final item = bookList[index];
-        return(Container(child: item,));
+        return (Container(
+          child: item,
+        ));
       },
       itemCount: bookList.length,
       itemWidth: 150,
-      itemHeight: 150,
+      itemHeight: 230,
       layout: SwiperLayout.STACK,
+    );
+  }
+  bodyPartDetail() {
+    return Container(
+      width: 150,
+      child: DropdownButtonFormField(
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.only(bottom: 2),
+            labelText: 'Body Part',
+          ),
+          items: <String>[
+            'Abdominal/Core',
+            'Back',
+            'Biceps',
+            'Calves',
+            'Chest',
+            'Forearms',
+            'Glutes',
+            'Hamstrings',
+            'Lats',
+            'Quads',
+            'Shoulders/Delts',
+            'Traps',
+            'Triceps',
+          ].map<DropdownMenuItem<String>>(
+                (String value) {
+              return DropdownMenuItem(
+                child: Text(value),
+                value: value,
+              );
+            },
+          ).toList(),
+          onChanged: (value) {
+            setState(() {
+              bodyPartQ = value;
+            });
+          },
+          value: bodyPartQ),
     );
   }
 }
 
-class padpenis extends StatelessWidget{
-  padpenis({Key key,this.day}) :super(key: key);
+class padpenis extends StatelessWidget {
+  padpenis({Key key, this.day}) : super(key: key);
 
   final String day;
 
@@ -184,9 +298,11 @@ class padpenis extends StatelessWidget{
   }
 
   hinge() {
-    return (Container);  }
-  testCont(Color color){
-    return(Container(
+    return (Container);
+  }
+
+  testCont(Color color) {
+    return (Container(
       width: 150,
       height: 150,
       color: color,
